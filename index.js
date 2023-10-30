37b6f05d1e39f7b2e237e587cd35e97f0ec11053
Ziyue Xu <xuziyue6@gmail.com>
2023-10-30 15:00:22 -0400

    fix typo in html

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  alert("DOM fully loaded and parsed");
-});
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   alert("DOM fully loaded and parsed");
+// });
